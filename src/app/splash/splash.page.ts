eec8eb0f6e33feba1ffcff2ddbfb4de910ab1ac0
Igor Araujo <igor.souza@equipe.voacolibri.com.br>
2026-04-20 23:09:21 -0300

    Início do Card 1.1 - Implementado estrutura da página inicial do aplicativo. Necessário melhorias e implementação das funções dos botões.

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonButton, CommonModule, FormsModule]
 })
 export class SplashPage implements OnInit {
+
+  isSplashVisible = true;
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.isSplashVisible = false;
+    }, 5000);
   }
 
 }
