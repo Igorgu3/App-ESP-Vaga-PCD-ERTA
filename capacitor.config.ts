e7bbde580f64e03972beaa77e998a29413cff936
Igor Araujo <igor.souza@equipe.voacolibri.com.br>
2026-06-06 12:27:38 -0300

    Modificação de itens para ficar responsivo em smartphones! Criação de pasta para exibição de ícone no aplicativo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app-vaga',
+  appName: 'Free Access',
   webDir: 'www'
 };
 
